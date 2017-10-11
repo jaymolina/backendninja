1f12f1b5b318f213ab5156b548210de3e0cd8a0e
Javier Molina <javier.molmar@gmail.com>
2017-10-11 20:06:15 +0200

    Generando mas info por pantalla
    Creando un nuevo password....

--- src/main/java/com/udemy/converter/TestCrypt.java
+++ src/main/java/com/udemy/converter/TestCrypt.java
@@ -6,6 +6,7 @@ public class TestCrypt {
 
     public static void main(String args[]){
         BCryptPasswordEncoder pe = new BCryptPasswordEncoder();
+        System.out.println(pe.encode("\n Generando password...\n"));
         System.out.println(pe.encode("user"));
     }
 

1cb629712da9969edad6e68c58a2b8992eb37d89
Javier Molina <javier.molmar@gmail.com>
2017-10-13 16:50:48 +0200

    Quitado codigo

--- src/main/java/com/udemy/converter/TestCrypt.java
+++ src/main/java/com/udemy/converter/TestCrypt.java
@@ -7,7 +7,5 @@ public class TestCrypt {
     public static void main(String args[]){
         BCryptPasswordEncoder pe = new BCryptPasswordEncoder();
         System.out.print(pe);
-        System.out.print("\n Prueba");
-        System.out.print("\n Prueba");
     }
 }

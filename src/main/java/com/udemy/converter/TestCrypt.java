68597660475a3f2f85c2bff0cf03a0763166c36e
Javier Molina <javier.molmar@gmail.com>
2017-10-13 13:00:49 +0200

    A

--- src/main/java/com/udemy/converter/TestCrypt.java
+++ src/main/java/com/udemy/converter/TestCrypt.java
@@ -7,6 +7,5 @@ public class TestCrypt {
     public static void main(String args[]){
         BCryptPasswordEncoder pe = new BCryptPasswordEncoder();
         System.out.println(pe.encode("user"));
-        
     }
 }

2491dac51cac67a14a600610077ab6fcc25ae6fb
Javier Molina <javier.molmar@gmail.com>
2017-10-13 11:40:47 +0200

    Quitando mensajes
    Quitando mensajes

--- src/main/java/com/udemy/converter/TestCrypt.java
+++ src/main/java/com/udemy/converter/TestCrypt.java
@@ -6,9 +6,6 @@ public class TestCrypt {
 
     public static void main(String args[]){
         BCryptPasswordEncoder pe = new BCryptPasswordEncoder();
-        System.out.println(pe.encode("\n Generando password...\n"));
         System.out.println(pe.encode("user"));
-        System.out.println("\n Password generado...");
     }
-
 }
